e46aebaf00489d8b7bde5af93ec3dbc85f9893d3
Yuri Neves Silveira <yurinevessilveira@gmail.com>
2015-06-08 21:19:48 -0300

    update factory

--- test/test.js
+++ test/test.js
@@ -8,21 +8,21 @@ var assert = require("assert");
 
 describe("Super",function() {
 
-	it("super",function(done) {
-		var proc = cp.spawn("node",[__dirname + "/sample/sample.js"],{ stdio: "pipe" });
-		var output = "";
-		proc.stdout.on("data",function(data) {
-			output += data.toString("utf8");
-		});
-		var error = "";
-		proc.stderr.on("data",function(data) {
-			error += data.toString("utf8");
-		});
-		proc.on("exit",function() {
-			assert.equal(error,"");
-			done();
-		});
-	});
+  it("super",function(done) {
+    var proc = cp.spawn("node",[__dirname + "/sample/sample.js"],{ stdio: "pipe" });
+    var output = "";
+    proc.stdout.on("data",function(data) {
+      output += data.toString("utf8");
+    });
+    var error = "";
+    proc.stderr.on("data",function(data) {
+      error += data.toString("utf8");
+    });
+    proc.on("exit",function() {
+      assert.equal(error,"");
+      done();
+    });
+  });
 
 });
 
